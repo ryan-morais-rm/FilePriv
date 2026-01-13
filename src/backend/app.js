149820de85e3e37725ae54d9d543f84926f7c5af
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-01-12 23:46:19 -0300

    Adição das rotas de arquivos

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors'); 
 const authRoutes = require('../backend/routes/authRoutes');
+const fileRoutes = require('../backend/routes/fileRoutes');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/usuarios', authRoutes);
+app.use('/arquivos', fileRoutes); 
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000 com estrutura organizada');
