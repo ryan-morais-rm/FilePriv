03188033a8b297d67b8af0be2f9ea131ba4072c0
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-01-12 23:51:16 -0300

    rota POST para subir os arquivos, ainda falta implementar o GET

--- src/backend/routes/fileRoutes.js
+++ src/backend/routes/fileRoutes.js
@@ -1,5 +1,26 @@
-
 // Preciso de rotas GET para fornecer o nome do usuário
 // Preciso de rotas GET que forneçam quantidade de arquivos armazenados e arquivos consultados
 // Preciso de rotas GET que forneçam nome do arquivo, data de upload e descrição do arquivo 
 // Preciso de rotas POST que forneçam nome do arquivo, data de upload e descrição do arquivo 
+
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const router = express.Router();
+const fileController = require('../controllers/fileController');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); 
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/upload', upload.single('arquivo'), fileController.uploadFile);
+
+module.exports = router;
