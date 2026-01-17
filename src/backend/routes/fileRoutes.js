19e3bd88dcb600b2d9dfc7f05867d437e53c12e3
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-01-17 00:15:59 -0300

    Adição de rota GET

--- src/backend/routes/fileRoutes.js
+++ src/backend/routes/fileRoutes.js
@@ -1,7 +1,6 @@
 // Preciso de rotas GET para fornecer o nome do usuário
 // Preciso de rotas GET que forneçam quantidade de arquivos armazenados e arquivos consultados
 // Preciso de rotas GET que forneçam nome do arquivo, data de upload e descrição do arquivo 
-// Preciso de rotas POST que forneçam nome do arquivo, data de upload e descrição do arquivo 
 
 const express = require('express');
 const multer = require('multer');
