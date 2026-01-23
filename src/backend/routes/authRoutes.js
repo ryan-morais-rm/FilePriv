35daf92cf69fdc99c8161b0e39fe1dbee9ee078a
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-01-23 15:14:03 -0300

    Rotas atualizadas, removendo os IDs desnecessários. Além da separação entre públicas e privadas

--- src/backend/routes/authRoutes.js
+++ src/backend/routes/authRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express'; 
 import userController from '../controllers/authController.js';
+import verificarToken from '../middlewares/authMiddleware.js';
 
 const authRouter = express.Router();
 
+// Rotas públicas
 authRouter.post('/cadastro', userController.createUser);
 authRouter.post('/login', userController.loginUser);
-authRouter.get('/perfil/:id', userController.consultUser);
+
+// Rota privada
+authRouter.get('/perfil/:id', verificarToken, userController.consultUser);
 
 
 export default authRouter;
--- src/backend/routes/fileRoutes.js
+++ src/backend/routes/fileRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import fileController from '../controllers/fileController.js';
+import verificarToken from '../middlewares/authMiddleware.js';
 
 const fileRouter = express.Router();
 
@@ -16,9 +17,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-fileRouter.post('/upload', upload.single('arquivo'), fileController.uploadFile);
-fileRouter.get('/download/:id', fileController.downloadFile);
-fileRouter.get('/armazenados/quantidade/:usuario_id', fileController.filesStored); 
-fileRouter.get('/armazenados/lista/:usuario_id', fileController.listUserFiles);
+fileRouter.post('/upload', verificarToken, upload.single('arquivo'), fileController.uploadFile);
+fileRouter.get('/download/:id', verificarToken, fileController.downloadFile);
+fileRouter.get('/armazenados/quantidade/', verificarToken, fileController.filesStored); 
+fileRouter.get('/armazenados/lista/', verificarToken, fileController.listUserFiles);
 
 export default fileRouter;
