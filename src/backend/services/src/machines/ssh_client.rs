294d1712c2c1087c1c24107a05de9478d7525934
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-01-31 23:59:11 -0300

    Configuração da sessão TCP

--- src/backend/services/src/machines/ssh_client.rs
+++ src/backend/services/src/machines/ssh_client.rs
@@ -1,25 +1,51 @@
-// Wrapper técnico do SFTP
+use std::net::TcpStream;
 use std::path::Path;
+use std::fs::File;
+use std::io::{Read, Write};
+use ssh2::Session;
 
 pub struct SSHClient {
-    // A sessão mantém a conexão TCP/Criptografia
-    session: Option<ssh2::Session>,
+    session: Session,
 }
 
 impl SSHClient {
-    // ADICIONADO: 'private_key_path'
-    pub fn connect(ipv4: &str, port: u16, user: &str, private_key_path: &Path) -> Result<Self, String> {
-        // 1. TcpStream::connect
-        // 2. Session::new
-        // 3. session.handshake
-        // 4. session.userauth_pubkey_file(...)
-        todo!()
+    // Conecta e Autentica via Chave Pública
+    pub fn connect(ipv4: &str, port: u16, user: &str, key_path: &Path) -> Result<Self, String> {
+        let tcp = TcpStream::connect(format!("{}:{}", ipv4, port))
+            .map_err(|e| format!("Falha TCP em {}: {}", ipv4, e))?;
+
+        let mut session = Session::new().map_err(|e| e.to_string())?;
+        
+        session.set_tcp_stream(tcp);
+        
+        session.handshake().map_err(|e| format!("Falha Handshake: {}", e))?;
+        
+        session.userauth_pubkey_file(user, None, key_path, None)
+            .map_err(|e| format!("Falha de Autenticação para {}: {}", user, e))?;
+
+        
+        if !session.authenticated() {
+            return Err("Autenticação rejeitada pelo servidor.".to_string());
+        }
+
+        Ok(Self { session })
     }
-    
-    pub fn send_file(&mut self, local_path: &Path, remote_path: &str) -> Result<(), String> {
-        // 1. self.session.sftp() -> Cria o canal SFTP
-        // 2. sftp.create(remote_path)
-        // 3. Ler arquivo local e escrever no remoto
-        todo!()
+
+    pub fn send_file(&self, local_path: &Path, remote_filename: &str) -> Result<(), String> {
+        let sftp = self.session.sftp()
+            .map_err(|e| format!("Falha ao iniciar SFTP: {}", e))?;
+
+        let remote_path = Path::new("uploads").join(remote_filename);        
+        
+        let mut remote_file = sftp.create(&remote_path)
+            .map_err(|e| format!("Não foi possível criar arquivo remoto {:?}: {}", remote_path, e))?;
+        
+        let mut local_file = File::open(local_path)
+            .map_err(|e| format!("Erro ao ler arquivo local: {}", e))?;]
+        
+        std::io::copy(&mut local_file, &mut remote_file)
+            .map_err(|e| format!("Erro durante transferência de dados: {}", e))?;
+
+        Ok(())
     }
 }
