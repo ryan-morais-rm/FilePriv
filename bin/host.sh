7aef541e84bd04e4301e6ed73426750378ce5821
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-03-06 23:14:55 -0300

    Comando que cria as chaves privadas e o certificado

--- bin/host.sh
+++ bin/host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Rode esse script para configurar as chaves privadas
-
 ssh-keygen -m PEM -t rsa -b 4096 -f ./storage_key -N ""
 
-
+# Esse comando serve para utilizar o HTTPS
+sudo npx http-server . -p 443 -S -C src/backend/https_pem/cert.pem -K src/backend/https_pem/key.pem
