344e3531cca2f88de927b935e1da53f468670e97
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2026-03-06 23:16:56 -0300

    Atualização das configurações do cors, adição do HTTPS, chaves privadas, certificados e dos módulos necessários

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,25 +1,41 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import https from 'https';
+import path from 'path'; 
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
 import authRouter from './routes/authRoutes.js';
 import fileRouter from './routes/fileRoutes.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const privateKey = fs.readFileSync('https_pem/key.pem', 'utf8');
+const certificate = fs.readFileSync('https_pem/cert.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
-app.use(cors());
+const app = express();
+const PORT = 443
+
+const httpsServer = https.createServer(credentials, app);
+
+app.use(cors({
+    origin: ['https://localhost', 'https://127.0.0.1'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
 app.use('/usuarios', authRouter);
 app.use('/arquivos', fileRouter);
 
-app.get('/', (req, res) => {
-    res.send('Servidor ativo');
-});
+app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.listen(PORT, () => {
-    console.log(`✅ Servidor rodando na porta ${PORT}`);
+httpsServer.listen(PORT, () => {
+    console.log(`API Node.js rodando em https://localhost:${PORT}`);
 });
